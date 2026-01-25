45141fc2780b85756dc4f485ba01a1446c6b7c72
ForsakendMJr <64882709+ForsakendMJr@users.noreply.github.com>
2026-01-25 08:41:38 +0000

    Path fixes..

--- app/api/guilds/[guildId]/settings/route.js
+++ app/api/guilds/[guildId]/settings/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import dbConnect from "@/lib/mongodb";
+import dbConnect from "../../../../lib/mongodb";
 import GuildSettings from "../../../../models/GuildSettings";
 
 export const dynamic = "force-dynamic";
